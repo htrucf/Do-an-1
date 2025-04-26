b8599a82474868d75d4c5b45b0e493cbc883abbc
htrucf <htrucf2004@gmail.com>
2025-04-27 01:09:43 +0700

    Update count_chief_complaint.py

--- code/count_chief_complaint.py
+++ code/count_chief_complaint.py
@@ -1,8 +1,11 @@
 import pandas as pd
 from collections import Counter
+import os
+
+os.chdir('C:/Users/LENOVO/Downloads') # THAY ĐỔI ĐƯỜNG DẪN ĐẾN THƯ MỤC CỦA BẠN
 
 # Bước 1: Đọc file Excel (giả sử sheet chứa dữ liệu cần xử lý có tên là 'triage_data')
-df = pd.read_excel('C:/Users/LENOVO/Downloads/triage_data.xlsx', sheet_name='chiefcomplaint')  # Thay đúng tên file và sheet
+df = pd.read_excel('./data/triage_data.xlsx', sheet_name='chiefcomplaint')  # Thay đúng tên file và sheet
 
 # Bước 2: Tách cụm từ bằng dấu phẩy, chuẩn hóa chữ thường, loại bỏ khoảng trắng
 all_phrases = []
@@ -17,7 +20,7 @@ counter = Counter(all_phrases)
 
 # Bước 4: Lưu thống kê ra file CSV
 counter_df = pd.DataFrame(counter.items(), columns=['Phrase', 'Count'])
-counter_df.to_csv('C:/Users/LENOVO/Downloads/complaint_extracted_check.csv', index=False)
+counter_df.to_csv('./data/count_complaint.csv', index=False)
 
 # In kết quả thống kê
 print(counter_df.head())  # In ra một vài dòng đầu của thống kê
